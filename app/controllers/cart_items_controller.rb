6229037cefcd210853b511887ca2617f64a3f43e
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-25 17:59:06 +0000

    Completed 200 OK in 166ms (Views: 90.6ms | ActiveRecord: 1.3ms)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,8 +1,19 @@
 class CartItemsController < ApplicationController
   def new
+    @cart_item = CartItem.new(product_id: params[product_id],
+    cart_id: :current_cart.id)
+    
   end
   
   def create
+    quantity = params[:cart_item][:quantity]
+    cart_item.quantity = quantity
+    if cart_item.save
+      flash[:notice] = 'アイテムがカートに追加されました'
+      redirect_to root_path
+    else
+      flash[:notice] = 'アイテムの追加に失敗しました'
+    end
   end
   
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
     validates :name,:price, presence: true
+    has_one :cart_items
 end
