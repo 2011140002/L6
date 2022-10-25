93295801a682d0cc7cf1fd88186470565ccc2d0f
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-25 18:21:54 +0000

    Completed 200 OK in 134ms (Views: 109.1ms | ActiveRecord: 0.6ms)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,15 +6,15 @@ class CartItemsController < ApplicationController
   end
   
   def create
-    qty = params[:cart_item][:qty]
-    cart_item = CartItem.new(product_id: params[:product_id],
-    cart_id: :current_cart, qty: qty)
-    if cart_item.save
-      flash[:notice] = 'アイテムがカートに追加されました'
-      redirect_to root_path
-    else
-      flash[:notice] = 'アイテムの追加に失敗しました'
-    end
+    # qty = params[:cart_item][:qty]
+    # cart_item = CartItem.new(product_id: params[:product_id],
+    # cart_id: :current_cart, qty: qty)
+    # if cart_item.save
+    #   flash[:notice] = 'アイテムがカートに追加されました'
+    #   redirect_to root_path
+    # else
+    #   flash[:notice] = 'アイテムの追加に失敗しました'
+    # end
   end
   
   def destroy
