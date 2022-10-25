7403f528d5e998724bb8dba68094ce539112816a
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-25 18:45:11 +0000

    Completed 200 OK in 123ms (Views: 103.8ms | ActiveRecord: 7.2ms)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,12 +6,9 @@ class CartItemsController < ApplicationController
   end
   
   def create
-    # qty = params[:cart_item][:qty]
-    # qty = 1
-    # cart_item = CartItem.new(product_id: params[:product_id],
-    # cart_id: :current_cart, qty: 1)
-    cart_item = CartItem.new(product_id: 4, cart_id: 1, qty: 1)
-    # debugger.log(cart_item)
+    cart_item.product = Product.find(5)
+    cart_item.cart = Cart.find(1)
+    cart_item.qty = 3
     if cart_item.save
       flash[:notice] = 'アイテムがカートに追加されました'
       redirect_to root_path
