db2d42c0f8ca840c825ddd5b0cae6d17c711e5fb
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-25 12:16:34 +0000

    Completed 200 OK in 366ms (Views: 351.0ms | ActiveRecord: 0.0ms)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,20 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
   
   def new
+    @product = Product.new
   end
   
   def create
+    product = Product.new(name: params[:product][:name],
+    price: params[:product][:price])
+    if product.save
+      redirect_to 'index'
+    else
+      flash[:notice] = "商品登録に失敗しました"
+    end
   end
   
   def destroy
