6ec25da7ae4db0725de4b0765614418f30e9c24f
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-25 18:25:49 +0000

    NoMethodError (undefined method `quantity' for #<CartItem:0x00007fc45d920440>):

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -10,6 +10,7 @@ class CartItemsController < ApplicationController
     # qty = 1
     cart_item = CartItem.new(product_id: params[:product_id],
     cart_id: :current_cart, qty: 1)
+    p cart_item
     if cart_item.save
       flash[:notice] = 'アイテムがカートに追加されました'
       redirect_to root_path
