440b81d1b4b2c8d58ce3de378f7fe45a73bb0f06
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-26 02:28:40 +0000

    Completed 200 OK in 60ms (Views: 47.7ms | ActiveRecord: 1.3ms)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
+    def L6
+      session[:c] ||= 0
+      session[:c] = session[:c].to_i + 1
+      render plain: session[:c]
+    end
+    
     private
-        def current_cart
-            if session[:cart_id]
-                cart = Cart.find(session[:cart_id])
-            else
-                cart = Cart.create
-                session[:cart_id] = cart.id
-            end
-            cart
+    def current_cart
+        if session[:cart_id]
+            cart = Cart.find(session[:cart_id])
+        else
+            cart = Cart.create
+            session[:cart_id] = cart.id
         end
-        helper_method :current_cart
+        cart
+    end
+    helper_method :current_cart
 end
