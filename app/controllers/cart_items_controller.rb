7c2e7e89ac505cecfee891f38cea0a2469215cc5
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-26 03:10:23 +0000

    Completed 200 OK in 42ms (Views: 38.3ms | ActiveRecord: 0.5ms)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -19,7 +19,7 @@ class CartItemsController < ApplicationController
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    render "cart#show"
+    redirect_to "cart#show"
   end
   
 end
